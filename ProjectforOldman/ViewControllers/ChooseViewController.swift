7f48633971caac93ee356c7e0538ed205ce846e7
peemgay <peempeem2@gmail.com>
2020-06-16 22:43:35 +0700

    top commit

---
+++ ProjectforOldman/ViewControllers/ChooseViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ChooseViewController.swift
+//  ProjectforOldman
+//
+//  Created by Peem on 16/6/2563 BE.
+//  Copyright © 2563 Peem. All rights reserved.
+//
+
+import UIKit
+
+class ChooseViewController: UITableViewController {
+
+    @IBOutlet weak var lbl: UILabel!
+    @IBOutlet weak var lbl1: UILabel!
+    @IBOutlet weak var lbl2: UILabel!
+    
+    @IBAction func stepper(_ sender: UIStepper)
+    {
+        lbl.text = String(sender.value)
+    }
+    @IBAction func stepper1(_ sender: UIStepper)
+    {
+        lbl1.text = String(sender.value)
+    }
+    @IBAction func stepper2(_ sender: UIStepper)
+    {
+        lbl2.text = String(sender.value)
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ProjectforOldman/ViewControllers/ForgotPasswordViewController.swift
+++ ProjectforOldman/ViewControllers/ForgotPasswordViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ForgotPasswordViewController: UITableViewController {
+    
+    
+    @IBOutlet weak var userEmailTextField: UITextField!
     @IBOutlet weak var send: UIButton!
     @IBOutlet weak var cancel: UIButton!
     
@@ -32,11 +35,47 @@ class ForgotPasswordViewController: UITableViewController {
         
        
     }
-    @IBAction func cancelTapButton(){
+    
+    @IBAction func cancelTapButton()
+    {
            let vc = storyboard?.instantiateViewController(identifier: "signin") as! SigninViewController
            vc.modalPresentationStyle = .fullScreen
            present(vc,animated: true)
-       }
+    }
+    
+    @IBAction func recoverButtonTapped(_ sender: AnyObject) {
+        
+        let userEmail = userEmailTextField.text
+        
+        User.confirmEmailTextField(userEmail) {
+            (success:Bool, error:NSError?) -> Void in
+            
+            if(success)
+            {
+                let successMessage = "Email message was sent to you at \(userEmail)"
+                self.displayMessage(successMessage)
+                return
+            }
+            
+            if(error != nil)
+            {
+                let errorMessage:String = error!.userInfo!["error"] as! String
+                self.displayMessage(errorMessage)
+            }
+        }
+    }
+        
+    func displayMessage(theMessage:String)
+    {
+        let myAlert = UIAlertController(title:"Alert", message:theMessage, preferredStyle: UIAlertController.Style.alert);
+        
+        let okAction = UIAlertAction(title: "OK", style:UIAlertAction.Style.default){
+            action in
+            self.dismiss(animated: true, completion:nil);
+        }
+        myAlert.addAction(okAction);
+        self.present(myAlert, animated:true, completion:nil);
+    }
+    
 }
-
 
